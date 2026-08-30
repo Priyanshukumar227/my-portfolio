2f473a19ea5894abc710ad31c7dfd56f9ace13a4
Priyanshu Kumar <kumar123priyanshu123p@gmail.com>
2026-08-30 22:42:47 +0530

    Fix import casing for contacts component

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,9 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Educations from './components/Educations'
-import Contacts from './components/Contacts'
+import Contacts from './components/contacts'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
